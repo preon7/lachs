491d75c7222ec63566ef7c7e1812016a82704e22
Preon7 <preon7@gmail.com>
2023-04-26 15:06:21 +0200

    move to current dir before execution

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "${DIR}"
+nadp asdesfsd
 npm install ejs nxapi body-parser winston
 cp tools/na.js node_modules/nxapi/dist/api/na.js
 npx nxapi nso auth
--- launch-Lachs-v0.7.sh
+++ launch-Lachs-v0.7.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-node Lachs-server.js &
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "${DIR}"
+node ./Lachs-server.js &
 
 until npx nxapi splatnet3 monitor saved_results --coop --update-interval 10; do
     echo monitor encountered an error, restarting
